721dfe74af551ef25beefdfc65e0796ba49fbca1
lyglade <lygla@DESKTOP-8E6UQQ7>
2019-01-16 00:36:28 +0800

    20190115-3

--- WebSpider/WebSpider.py
+++ WebSpider/WebSpider.py
@@ -1,19 +1,27 @@
 # -*- coding:utf-8 -*-
 import os
 import requests
+import re
 from bs4 import BeautifulSoup
 
 def spider():
-    url = "http://bj.grfy.net/"
+    url = "http://www.airenti55.net/"
     #proxies = {"http": "http://172.17.18.80:8080", "https": "https://172.17.18.80:8080"}
     #r = requests.get(url, proxies=proxies)
     r = requests.get(url)
     html = r.content
     soup = BeautifulSoup(html, "lxml")
-    divs = soup.find_all("div", class_="content")  
-    print(len(divs))
+    uls = soup.find_all("ul", class_="photo")
+    print(uls)
+    aa=[]
+    for web_url in uls:
+        urls=re.findall(r'href=".*?"', str(web_url))
+        aa.append(urls)
+        
+          
+    
     #print(soup)
-    print(divs)
+    print(aa)
 
 if __name__ == "__main__":
     spider()
